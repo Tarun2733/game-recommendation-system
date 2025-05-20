a9e58dedfd96e830ddb6e2292347780db063b7d5
Tarun <taruntiwari.4113@gmail.com>
2025-05-20 18:51:00 +0530

    model.pkl file added

--- backend/gamerecom/views.py
+++ backend/gamerecom/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def recommend_game(request):
     return HttpResponse("This is the game recommendation page.")
