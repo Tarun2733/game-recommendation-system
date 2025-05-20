73eb51979e8babf68414345df61a814de3ebb1c6
Tarun <taruntiwari.4113@gmail.com>
2025-05-20 19:25:42 +0530

    Made a get request with pkl model intergration

--- backend/gamerecom/views.py
+++ backend/gamerecom/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import  JsonResponse
+import pickle
+import pandas as pd
+import os
+from django.conf import settings
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#load pkl model
+#using settings.BASE_DIR to get the path of the model
+
+MODEL_PATH = os.path.join(settings.BASE_DIR, 'gamerecom', 'models', 'game_recommender.pkl')
+with open(MODEL_PATH, 'rb') as file:
+    model = pickle.load(file)
+
+#load data
+tagged = model['tagged']
+similarity_matrix = model['similarity_matrix']
+
+#recommendation function
+def recommend(game):
+    g = game.lower()
+    idx = tagged[tagged['name'].str.lower() == g].index[0]
+    d = similarity_matrix[idx]
+    lst = sorted(list(enumerate(d)), reverse=True, key=lambda x: x[1])[1:6]
+    recoms = [tagged.iloc[i[0]]['name'] for i in lst]
+    return recoms
+
+
+
 
 # Create your views here.
-
-
+@api_view(['GET'])
 def recommend_game(request):
-    return HttpResponse("This is the game recommendation page.")
+    # return HttpResponse("This is the game recommendation page.")
+    game = request.GET.get('game')
+    if not game:
+        return JsonResponse({'error': 'No game provided'}, status=400)
+
+    try:
+        recommendations = recommend(game)
+        return JsonResponse({'recommendations': recommendations})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
